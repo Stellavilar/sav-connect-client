1c88649a096788f6bcd8c17bc972bd2ff812d3db
Stella Vilar <vilar.stella@gmail.com>
2021-03-23 17:23:57 +0100

    Configure for github-page

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import { HashRouter as Router } from 'react-router-dom';
 import App from './components/App';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://ec2-3-84-161-129.compute-1.amazonaws.com/';
+// axios.defaults.baseURL = 'http://ec2-3-84-161-129.compute-1.amazonaws.com/';
 // axios.defaults.baseURL = 'http://localhost:9090/';
+axios.defaults.baseURL = 'https://d2hdqxhqxz4tb.cloudfront.net/';
+
 
 const rootReactElement = () => {
   return (
