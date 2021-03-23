5190cfddc55b71363ebb3d39a8c5a2ddb3d849c3
Stella Vilar <vilar.stella@gmail.com>
2021-03-23 20:26:18 +0100

    Update axios base url

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://ec2-3-84-161-129.compute-1.amazonaws.com/';
 // axios.defaults.baseURL = 'http://localhost:9090/';
-axios.defaults.baseURL = 'https://d2hdqxhqxz4tb.cloudfront.net/';
+axios.defaults.baseURL = 'https://d1w95kht64fc6l.cloudfront.net/';
 
 
 const rootReactElement = () => {
